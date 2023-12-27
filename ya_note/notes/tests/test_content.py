24b5887f4e7bd45aefd58d7c4001d04ec6848c6d
Владимир Матасов <dragon111@mail.ru>
2023-12-28 02:43:05 +0300

    Add content tests

---
+++ ya_note/notes/tests/test_content.py
@@ -0,0 +1,70 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+
+
+User = get_user_model()
+
+
+class TestListNotes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author_1 = User.objects.create(username='У. Черчиль')
+        cls.author_2 = User.objects.create(username='А. Веллингтон')
+
+        all_notes = [
+            Note(
+                title=f'Заголовок {index}',
+                text='Просто текст.',
+                slug=f'Slug_{index}',
+                author=cls.author_1,
+            )
+            if index % 2 == 1 else
+            Note(
+                title=f'Заголовок {index}',
+                text='Просто текст.',
+                slug=f'Slug_{index}',
+                author=cls.author_2,
+            )
+            for index in range(4)
+        ]
+        cls.notes = Note.objects.bulk_create(all_notes)
+
+    def test_notes_one_author(self):
+        self.client.force_login(self.author_1)
+        response = self.client.get(reverse('notes:list'))
+        object_list = response.context['object_list']
+        self.assertNotEqual(object_list[0].author, self.author_2)
+
+    def test_object_is_note(self):
+        self.client.force_login(self.author_1)
+        response = self.client.get(reverse('notes:list'))
+        object_list = response.context['object_list']
+        self.assertIsInstance(object_list[0], Note)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='У. Черчиль')
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Просто текст.',
+            slug='Slug',
+            author=cls.author,
+        )
+        cls.urls = [
+            ('notes:add', None),
+            ('notes:edit', (cls.note.slug,)),
+        ]
+
+    def test_form_unclude_page(self):
+        self.client.force_login(self.author)
+        for name, args in self.urls:
+            with self.subTest(name=name):
+                response = self.client.get(reverse(name, args=args))
+                self.assertIn('form', response.context)
