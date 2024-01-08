41d84ba125b9986dfcc4470eb74a8f7d4f170bda
Vladimir <dragon111@mail.ru>
2024-01-08 21:41:38 +0300

    Start correct logic tests by Ya_news

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,90 +1,90 @@
-import pytest
+# import pytest
 
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 
-from django.conf import settings
-from django.urls import reverse
+# from django.conf import settings
+# from django.urls import reverse
 
-from news.models import Comment, News
-
-
-# assert response.status_code != 200, response.context['form'].errors
-@pytest.mark.django_db
-def test_vount_news_on_home_page(client):
-    [
-        News.objects.create(
-            title='Заголовок',
-            text='Текст заметки',
-            date=datetime.today() - timedelta(days=index)
-        )
-        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-    ]
-
-    # Act
-    response = client.get(reverse('news:home'))
-    object_list = response.context['object_list']
-    news_count = len(object_list)
-
-    assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
+# from news.models import Comment, News
 
 
-@pytest.mark.django_db
-def test_sort_news_on_home_page(client):
-    [
-        News.objects.create(
-            title='Заголовок',
-            text='Текст заметки',
-            date=datetime.today() - timedelta(days=index)
-        )
-        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-    ]
+# # assert response.status_code != 200, response.context['form'].errors
+# @pytest.mark.django_db
+# def test_vount_news_on_home_page(client):
+#     [
+#         News.objects.create(
+#             title='Заголовок',
+#             text='Текст заметки',
+#             date=datetime.today() - timedelta(days=index)
+#         )
+#         for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+#     ]
 
-    # Act
-    response = client.get(reverse('news:home'))
-    object_list = response.context['object_list']
-    all_dates = [news.date for news in object_list]
-    sorted_dates = sorted(all_dates, reverse=True)
+#     # Act
+#     response = client.get(reverse('news:home'))
+#     object_list = response.context['object_list']
+#     news_count = len(object_list)
 
-    assert all_dates == sorted_dates
-
-
-@pytest.mark.django_db
-def test_sort_comments(author, client, news):
-    [
-        Comment.objects.create(
-            news=news,
-            author=author,
-            text='Текст комментария',
-            created=datetime.today() - timedelta(days=index)
-        )
-        for index in range(2)
-    ]
-    url = reverse('news:detail', args=(news.id,))
-
-    # Act
-    response = client.get(url)
-    all_comments = response.context['news'].comment_set.all()
-    all_dates = [comment.created for comment in all_comments]
-    sorted_dates = sorted(all_dates)
-
-    assert all_dates == sorted_dates
+#     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-@pytest.mark.django_db
-def test_anonymous_client_has_no_form(client, news):
-    url = reverse('news:detail', args=(news.id,))
+# @pytest.mark.django_db
+# def test_sort_news_on_home_page(client):
+#     [
+#         News.objects.create(
+#             title='Заголовок',
+#             text='Текст заметки',
+#             date=datetime.today() - timedelta(days=index)
+#         )
+#         for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+#     ]
 
-    # Act
-    response = client.get(url)
+#     # Act
+#     response = client.get(reverse('news:home'))
+#     object_list = response.context['object_list']
+#     all_dates = [news.date for news in object_list]
+#     sorted_dates = sorted(all_dates, reverse=True)
 
-    assert 'form' not in response.context
+#     assert all_dates == sorted_dates
 
 
-@pytest.mark.django_db
-def test_authorized_client_has_form(news, reader_client):
-    url = reverse('news:detail', args=(news.id,))
+# @pytest.mark.django_db
+# def test_sort_comments(author, client, news):
+#     [
+#         Comment.objects.create(
+#             news=news,
+#             author=author,
+#             text='Текст комментария',
+#             created=datetime.today() - timedelta(days=index)
+#         )
+#         for index in range(2)
+#     ]
+#     url = reverse('news:detail', args=(news.id,))
 
-    # Act
-    response = reader_client.get(url)
+#     # Act
+#     response = client.get(url)
+#     all_comments = response.context['news'].comment_set.all()
+#     all_dates = [comment.created for comment in all_comments]
+#     sorted_dates = sorted(all_dates)
 
-    assert 'form' in response.context
+#     assert all_dates == sorted_dates
+
+
+# @pytest.mark.django_db
+# def test_anonymous_client_has_no_form(client, news):
+#     url = reverse('news:detail', args=(news.id,))
+
+#     # Act
+#     response = client.get(url)
+
+#     assert 'form' not in response.context
+
+
+# @pytest.mark.django_db
+# def test_authorized_client_has_form(news, reader_client):
+#     url = reverse('news:detail', args=(news.id,))
+
+#     # Act
+#     response = reader_client.get(url)
+
+#     assert 'form' in response.context
--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,69 +1,73 @@
-import pytest
-
 from http import HTTPStatus
 
 from django.urls import reverse
+import pytest
 
 from news.forms import BAD_WORDS, WARNING
 from news.models import Comment
 
 
-@pytest.mark.django_db
-def test_not_availability_create_comment_for_anonymous_client(client, news):
-    url = reverse('news:detail', args=(news.id,))
+BAD_WORD_DATA = 'Текст, {bad_word}, остаток текста'
+
+
+# def test_not_availability_create_comment_for_anonymous_client(client, news):
+#     url = reverse('news:detail', args=(news.id,))
+
+#     # Act
+#     response = client.get(url)
+
+#     assert 'form' not in response.context
+
+
+# @pytest.mark.django_db
+# def test_availability_create_comment_for_user(news, reader_client):
+#     url = reverse('news:detail', args=(news.id,))
+
+#     # Act
+#     response = reader_client.get(url)
+
+#     assert 'form' in response.context
+
+
+@pytest.mark.parametrize(
+    'bad_word', BAD_WORDS,
+)
+def test_by_bad_words(client_author, NEWS_DETAIL, bad_word):
+    url = NEWS_DETAIL
+    # bad_word_data = {'text': f'Текст, {BAD_WORD}, остаток текста'}
+    bad_word_data = {'text': BAD_WORD_DATA.format(bad_word=bad_word)}
 
     # Act
-    response = client.get(url)
-
-    assert 'form' not in response.context
-
-
-@pytest.mark.django_db
-def test_availability_create_comment_for_user(news, reader_client):
-    url = reverse('news:detail', args=(news.id,))
-
-    # Act
-    response = reader_client.get(url)
-
-    assert 'form' in response.context
-
-
-@pytest.mark.django_db
-def test_by_bad_words(author_client, news):
-    url = reverse('news:detail', args=(news.id,))
-    bad_words_data = {'text': f'Текст, {BAD_WORDS[0]}, остаток текста'}
-
-    # Act
-    response = author_client.post(url, data=bad_words_data)
+    response = client_author.post(url, data=bad_word_data)
     comments_count = Comment.objects.count()
 
     assert response.context['form'].errors['text'][0] == WARNING
     assert comments_count == 0
 
 
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    'name',
-    ('news:edit', 'news:delete')
-)
-def test_pages_availability_for_author(author_client, comment, name):
-    url = reverse(name, args=(comment.id,))
+# @pytest.mark.django_db
+# @pytest.mark.parametrize(
+#     'name',
+#     ('news:edit', 'news:delete')
+# )
+# def test_pages_availability_for_author(author_client, comment, name):
+#     url = reverse(name, args=(comment.id,))
 
-    # Act
-    response = author_client.get(url)
+#     # Act
+#     response = author_client.get(url)
 
-    assert response.status_code == HTTPStatus.OK
+#     assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    'name',
-    ('news:edit', 'news:delete')
-)
-def test_pages_no_availability_for_reader(comment, name, reader_client):
-    url = reverse(name, args=(comment.id,))
+# @pytest.mark.django_db
+# @pytest.mark.parametrize(
+#     'name',
+#     ('news:edit', 'news:delete')
+# )
+# def test_pages_no_availability_for_reader(comment, name, reader_client):
+#     url = reverse(name, args=(comment.id,))
 
-    # Act
-    response = reader_client.get(url)
+#     # Act
+#     response = reader_client.get(url)
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
+#     assert response.status_code == HTTPStatus.NOT_FOUND
