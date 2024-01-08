2ef5e1a23dbddd8927527e9146d595d6339c3002
Владимир Матасов <dragon111@mail.ru>
2024-01-09 00:55:30 +0300

    Correct logic tests by Ya_news

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,6 +1,5 @@
 from http import HTTPStatus
 
-from django.urls import reverse
 import pytest
 
 from news.forms import BAD_WORDS, WARNING
@@ -8,33 +7,13 @@ from news.models import Comment
 
 
 BAD_WORD_DATA = 'Текст, {bad_word}, остаток текста'
+WORDS_DATA = 'Текст комментария'
+WORDS_DATA_NEW = 'Новый текст комментария'
 
 
-# def test_not_availability_create_comment_for_anonymous_client(client, news):
-#     url = reverse('news:detail', args=(news.id,))
-
-#     # Act
-#     response = client.get(url)
-
-#     assert 'form' not in response.context
-
-
-# @pytest.mark.django_db
-# def test_availability_create_comment_for_user(news, reader_client):
-#     url = reverse('news:detail', args=(news.id,))
-
-#     # Act
-#     response = reader_client.get(url)
-
-#     assert 'form' in response.context
-
-
-@pytest.mark.parametrize(
-    'bad_word', BAD_WORDS,
-)
-def test_by_bad_words(client_author, NEWS_DETAIL, bad_word):
-    url = NEWS_DETAIL
-    # bad_word_data = {'text': f'Текст, {BAD_WORD}, остаток текста'}
+@pytest.mark.parametrize('bad_word', BAD_WORDS)
+@pytest.mark.parametrize('url', [pytest.lazy_fixture('NEWS_DETAIL')])
+def test_by_bad_words(client_author, bad_word, url):
     bad_word_data = {'text': BAD_WORD_DATA.format(bad_word=bad_word)}
 
     # Act
@@ -45,29 +24,63 @@ def test_by_bad_words(client_author, NEWS_DETAIL, bad_word):
     assert comments_count == 0
 
 
-# @pytest.mark.django_db
-# @pytest.mark.parametrize(
-#     'name',
-#     ('news:edit', 'news:delete')
-# )
-# def test_pages_availability_for_author(author_client, comment, name):
-#     url = reverse(name, args=(comment.id,))
+@pytest.mark.parametrize('url', [pytest.lazy_fixture('NEWS_DETAIL')])
+def test_anonymous_client_cant_create_comment(client_anonymous, url):
+    form_data = {'text': WORDS_DATA}
+    # Act
+    response = client_anonymous.post(url, data=form_data)
 
-#     # Act
-#     response = author_client.get(url)
-
-#     assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.FOUND
+    assert Comment.objects.count() == 0
 
 
-# @pytest.mark.django_db
-# @pytest.mark.parametrize(
-#     'name',
-#     ('news:edit', 'news:delete')
-# )
-# def test_pages_no_availability_for_reader(comment, name, reader_client):
-#     url = reverse(name, args=(comment.id,))
+@pytest.mark.parametrize('url', [pytest.lazy_fixture('NEWS_DETAIL')])
+def test_client_can_create_comment(client_reader, url):
+    form_data = {'text': WORDS_DATA}
+    # Act
+    response = client_reader.post(url, data=form_data)
 
-#     # Act
-#     response = reader_client.get(url)
+    assert response.status_code == HTTPStatus.FOUND
+    assert Comment.objects.count() == 1
+    assert Comment.objects.get().text == form_data['text']
 
-#     assert response.status_code == HTTPStatus.NOT_FOUND
+
+@pytest.mark.parametrize('url', [pytest.lazy_fixture('COMMENT_EDIT')])
+def test_author_can_edit_your_comment(client_author, url, comment):
+    form_new_data = {'text': WORDS_DATA_NEW}
+    # Act
+    response = client_author.post(url, data=form_new_data)
+
+    assert response.status_code == HTTPStatus.FOUND
+    assert Comment.objects.count() == 1
+    assert Comment.objects.get().text == form_new_data['text']
+
+
+@pytest.mark.parametrize('url', [pytest.lazy_fixture('COMMENT_DELETE')])
+def test_author_can_delete_your_comment(client_author, url, comment):
+    # Act
+    response = client_author.post(url)
+
+    assert response.status_code == HTTPStatus.FOUND
+    assert Comment.objects.count() == 0
+
+
+@pytest.mark.parametrize('url', [pytest.lazy_fixture('COMMENT_EDIT')])
+def test_reader_cant_edit_authors_comment(client_reader, url, comment):
+    form_new_data = {'text': WORDS_DATA_NEW}
+    # Act
+    response = client_reader.post(url, data=form_new_data)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert Comment.objects.count() == 1
+    assert Comment.objects.get().text != form_new_data['text']
+
+
+@pytest.mark.parametrize('url', [pytest.lazy_fixture('COMMENT_DELETE')])
+def test_reader_cant_delete_authors_comment(client_reader, url, comment):
+    # Act
+    response = client_reader.post(url)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert Comment.objects.count() == 1
+    assert Comment.objects.get().text == WORDS_DATA
