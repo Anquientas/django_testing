dba5c52909ef8bfa6e536f0de0a6b1d60a58ae31
Владимир Матасов <dragon111@mail.ru>
2024-01-09 23:30:26 +0300

    Change comparisons parameters in logic tests by Ya_news

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 import pytest
 
 from news.forms import BAD_WORDS, WARNING
-from news.models import Comment
+from news.models import Comment, News
 
 
 BAD_WORD_DATA = {'text': 'Текст, {bad_word}, остаток текста'}
@@ -43,8 +43,10 @@ def test_client_can_create_comment(client_reader,
 
     assert response.status_code == HTTPStatus.FOUND
     assert Comment.objects.count() == 1
-    assert Comment.objects.get().text == FORM_DATA['text']
-    assert Comment.objects.get().author == reader
+    comment_new = Comment.objects.get()
+    assert comment_new.text == FORM_DATA['text']
+    assert comment_new.news == News.objects.get()
+    assert comment_new.author == reader
 
 
 def test_author_can_edit_your_comment(client_author,
@@ -56,8 +58,10 @@ def test_author_can_edit_your_comment(client_author,
         data=FORM_NEW_DATA
     ).status_code == HTTPStatus.FOUND
     assert Comment.objects.count() == 1
-    assert Comment.objects.get().text == FORM_NEW_DATA['text']
-    assert Comment.objects.get().author == author
+    comment_edit = Comment.objects.get()
+    assert comment_edit.text == FORM_NEW_DATA['text']
+    assert comment_edit.news == News.objects.get()
+    assert comment_edit.author == author
 
 
 def test_author_can_delete_your_comment(client_author,
@@ -70,25 +74,35 @@ def test_author_can_delete_your_comment(client_author,
 
 
 def test_reader_cant_edit_authors_comment(client_reader,
-                                          reader,
                                           comment_edit_url,
                                           comment):
+    text_old = comment.text
+    news_old = comment.news
+    author_old = comment.author
     assert client_reader.post(
         comment_edit_url,
         data=FORM_NEW_DATA
     ).status_code == HTTPStatus.NOT_FOUND
     assert Comment.objects.count() == 1
-    assert Comment.objects.get().text != FORM_NEW_DATA['text']
-    assert Comment.objects.get().author != reader
+    comment_cant_edit = Comment.objects.get()
+    assert comment_cant_edit.text == text_old
+    assert comment_cant_edit.news == news_old
+    assert comment_cant_edit.author == author_old
+    # assert Comment.objects.get().text != FORM_NEW_DATA['text']
+    # assert Comment.objects.get().author != reader
 
 
 def test_reader_cant_delete_authors_comment(client_reader,
-                                            reader,
                                             comment_delete_url,
                                             comment):
+    text_old = comment.text
+    news_old = comment.news
+    author_old = comment.author
     assert client_reader.post(
         comment_delete_url
     ).status_code == HTTPStatus.NOT_FOUND
     assert Comment.objects.count() == 1
-    assert Comment.objects.get().text == FORM_DATA['text']
-    assert Comment.objects.get().author != reader
+    comment_cant_delete = Comment.objects.get()
+    assert comment_cant_delete.text == text_old
+    assert comment_cant_delete.news == news_old
+    assert comment_cant_delete.author == author_old
