049068a9011417186dd6c1537d5e575cc9c82f0f
Владимир Матасов <dragon111@mail.ru>
2024-01-09 22:18:08 +0300

    Correct content tests by Ya_note

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,6 +1,7 @@
 from notes.models import Note
-from .base import Overall
-from .urls import (
+from notes.forms import NoteForm
+from .base import (
+    BaseTestCase,
     NOTE_SLUG,
     NOTES_ADD,
     NOTES_EDIT,
@@ -8,11 +9,11 @@ from .urls import (
 )
 
 
-class Base(Overall):
+class Base(BaseTestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Overall.setUpTestData()
+        BaseTestCase.setUpTestData()
 
         all_notes = [
             Note(
@@ -39,37 +40,21 @@ class TestListNotes(Base):
         response = self.client_author.get(NOTES_LIST)
         notes = response.context['object_list']
         self.assertIn(self.note, notes)
-        note = notes.get(slug=NOTE_SLUG)
+        note = notes.get(id=self.note.id)
         self.assertEqual(note.title, self.note.title)
         self.assertEqual(note.text, self.note.text)
         self.assertEqual(note.author, self.note.author)
+        self.assertEqual(note.slug, self.note.slug)
 
-    def test_object_in_list_objects_is_note(self):
-        response = self.client_author.get(NOTES_LIST)
+    def test_notes_not_available_for_another_author(self):
+        response = self.client_reader.get(NOTES_LIST)
         notes = response.context['object_list']
-        self.assertIn(self.note, notes)
-        note = notes.get(slug=NOTE_SLUG)
-        self.assertEqual(note.title, self.note.title)
-        self.assertEqual(note.text, self.note.text)
-        self.assertEqual(note.author, self.note.author)
-
-
-class TestDetailPage(Base):
+        self.assertNotIn(self.note, notes)
 
     def test_form_include_page(self):
         urls = (NOTES_ADD, NOTES_EDIT)
         for url in urls:
             with self.subTest(url=url):
-                self.assertIn('form', self.client_author.get(url).context)
-                self.assertIn(
-                    'title',
-                    self.client_author.get(url).context['form'].fields.keys()
-                )
-                self.assertIn(
-                    'text',
-                    self.client_author.get(url).context['form'].fields.keys()
-                )
-                self.assertIn(
-                    'slug',
-                    self.client_author.get(url).context['form'].fields.keys()
-                )
+                context = self.client_author.get(url).context
+                self.assertIn('form', context)
+                self.assertIs(type(context['form']), NoteForm)
